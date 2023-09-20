61e3eab28af884f66d7c43b56594b12a854c4aea
Mehmet Emre Şahin <memresahin40@gmail.com>
2023-09-20 15:56:24 +0300

    menu related with functionality

--- Menu.java
+++ Menu.java
@@ -8,22 +8,27 @@ public class Menu {
     public static void main(String[] args) {
         Scanner userIn = new Scanner(System.in);
         while(true){
+            int[] arr = new int[10];
+            for (int i = 0; i < 10; i++) {
+                arr[i] = (int)(100*Math.random());
+            }
             System.out.println("1- Find minimum of an array\n2- Find maximum of an array\n3- Find average of an array\n4- Find the sum of elements with odd-numbered indexes \n5- Find the sum of elements with even-numbered indexes\n6- Exit\nPlease select your action");
             int choice = userIn.nextInt();
             if(choice == 1){
 
+                GameOperations.ArrayMin(arr);
             }
             else if(choice == 2){
-
+                GameOperations.ArrayMax(arr);
             }
             else if(choice == 3){
-
+                GameOperations.Average(arr);
             }
             else if(choice == 4){
-                
+                GameOperations.oddIndex(arr);
             }
             else if(choice == 5){
-                
+                GameOperations.evenIndex(arr);
             }
             else if(choice== 6){
                 userIn.close();
