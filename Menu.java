f2f851409117d7dab6f65fcf8d9340ec4b3e739d
Ahmet Kenan Ataman <ataman.ahmet.kenan@gmail.com>
2023-09-20 15:50:17 +0300

    menu fixed

--- Menu.java
+++ Menu.java
@@ -8,7 +8,7 @@ public class Menu {
     public static void main(String[] args) {
         Scanner userIn = new Scanner(System.in);
         while(true){
-            System.out.println("1- Find minimum of an array\n2- Find maximum of an array\n3- Find average of an array\n4- Find the sum of elements with odd- and even-numbered indexes\n5- Exit\nPlease select your action");
+            System.out.println("1- Find minimum of an array\n2- Find maximum of an array\n3- Find average of an array\n4- Find the sum of elements with odd-numbered indexes \n5- Find the sum of elements with even-numbered indexes\n6- Exit\nPlease select your action");
             int choice = userIn.nextInt();
             if(choice == 1){
 
@@ -17,12 +17,15 @@ public class Menu {
 
             }
             else if(choice == 3){
-                
+
             }
             else if(choice == 4){
                 
             }
             else if(choice == 5){
+                
+            }
+            else if(choice== 6){
                 userIn.close();
                 break;
             }
